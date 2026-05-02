453e17c5ff0b4b33de83ed7624110cb853f56d4a
Alexandre <alexandre.martel75004@gmail.com>
2026-05-02 17:36:09 +0200

    finish NetworkGrad.rs

--- src/nn/network_grad.rs
+++ src/nn/network_grad.rs
@@ -9,7 +9,40 @@ pub struct NetworkGrad {
 
 impl NetworkGrad {
     pub fn zeros(device: &Device) -> Result<Self> {
-        
+        Ok(Self {
+            dw1: Tensor::zeros((16,3), candle_core::DType::F32, device)?,
+            db1: Tensor::zeros(16, candle_core::DType::F32, device)?,
+            dw2: Tensor::zeros((4,16), candle_core::DType::F32, device)?,
+            db2: Tensor::zeros(4, candle_core::DType::F32, device)?,
+        })
     }
+
+    pub fn scale(&self, scalar: f64) -> Result<Self> {
+        Ok(Self {
+            dw1: self.dw1.affine(scalar, 0.0)?,
+            db1: self.db1.affine(scalar, 0.0)?,
+            dw2: self.dw2.affine(scalar, 0.0)?,
+            db2: self.db2.affine(scalar, 0.0)?,
+        })
+    }
+
+    pub fn add(&self, other: &Self) -> Result<Self> {
+        Ok(Self {
+            dw1: (&self.dw1 + &other.dw1)?,
+            db1: (&self.db1 + &other.db1)?,
+            dw2: (&self.dw2 + &other.dw2)?,
+            db2: (&self.db2 + &other.db2)?,
+        })
+    }
+
+    pub fn from_vec(grads: Vec<Tensor>) -> Result<Self> {
+        Ok(Self {
+            dw1: grads[0].copy()?,
+            db1: grads[1].copy()?,
+            dw2: grads[2].copy()?,
+            db2: grads[3].copy()?,
+        })
+    }
+
 }
 
